85bc2d8b546f83ea744677b5b56b2e6fa2161c96
kolya5544 <kolya@nk.ax>
2026-05-15 17:20:46 +0300

    fix(backend): allow E2E frontend origin

--- Program.cs
+++ Program.cs
@@ -114,7 +114,9 @@ namespace KuSaFeBackend
                     policy
                         .WithOrigins(
                             "http://localhost:5173",
-                            "http://127.0.0.1:5173"
+                            "http://127.0.0.1:5173",
+                            "http://localhost:5174",
+                            "http://127.0.0.1:5174"
                         )
                         .AllowAnyHeader()
                         .AllowAnyMethod()
